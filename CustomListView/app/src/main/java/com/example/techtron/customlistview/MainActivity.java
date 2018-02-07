0dff6364b831f42523f02a7ef22958537847b0a4
Phillip <phillipables1@gmail.com>
2018-02-07 18:56:46 -0500

    initialize variables and import pictures

--- CustomListView/app/src/main/java/com/example/techtron/customlistview/MainActivity.java
+++ CustomListView/app/src/main/java/com/example/techtron/customlistview/MainActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    int [] IMAGES = { R.drawable.bigie, R.drawable.gandhi, R.drawable.cobain, R.drawable.musk, R.drawable.swazey, R.drawable.patrick, R.drawable.dog};
+    String[] NAMES = {"Biggie Smalls", "Mahatma Gandhi", "Kurt Cobain", "Elon Musk", "Patrick Swazye", "Patrick Star", "Dog"};
+    String[] DESCRIPTIONS = {"Rapper", "Activist", "Rock Star", "SpaceMan", "Bouncer", "Actor", "Mans Best Friend"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
