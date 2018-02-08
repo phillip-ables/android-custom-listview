7617ce252fc38311d5a5f8ea8b0da9f89d242e5d
Phillip <phillipables1@gmail.com>
2018-02-07 19:09:19 -0500

    CustomAdapter Over rides to get Count Item Id View

--- CustomListView/app/src/main/java/com/example/techtron/customlistview/MainActivity.java
+++ CustomListView/app/src/main/java/com/example/techtron/customlistview/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.techtron.customlistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
@@ -19,7 +21,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class CustomerAdapter extends BaseAdapter{
-        
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+
     }
+
 }
