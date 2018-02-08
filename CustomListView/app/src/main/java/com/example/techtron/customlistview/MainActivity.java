6131e32ee1a4ac22fee5b89139e91b377cacbbfe
Phillip <phillipables1@gmail.com>
2018-02-07 19:01:20 -0500

    implement base class adapter with the help of a subclass adapter

--- CustomListView/app/src/main/java/com/example/techtron/customlistview/MainActivity.java
+++ CustomListView/app/src/main/java/com/example/techtron/customlistview/MainActivity.java
@@ -2,10 +2,11 @@ package com.example.techtron.customlistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    int [] IMAGES = { R.drawable.bigie, R.drawable.gandhi, R.drawable.cobain, R.drawable.musk, R.drawable.swazey, R.drawable.patrick, R.drawable.dog};
+    int[] IMAGES = { R.drawable.bigie, R.drawable.ghandi, R.drawable.cobain, R.drawable.musk, R.drawable.swazey, R.drawable.patrick, R.drawable.dog};
     String[] NAMES = {"Biggie Smalls", "Mahatma Gandhi", "Kurt Cobain", "Elon Musk", "Patrick Swazye", "Patrick Star", "Dog"};
     String[] DESCRIPTIONS = {"Rapper", "Activist", "Rock Star", "SpaceMan", "Bouncer", "Actor", "Mans Best Friend"};
 
@@ -15,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listView=(ListView)findViewById(R.id.listView);
+
+    }
+
+    class CustomerAdapter extends BaseAdapter{
+        
     }
 }
